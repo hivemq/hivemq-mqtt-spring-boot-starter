f3af9f9ef235658edfef2424bc12848f3337e8ec
skobow <sven.kobow@hivemq.com>
2024-02-14 17:39:29 +0100

    Fix typo

--- hivemq-mqtt-spring-boot-autoconfigure/src/main/java/net/skobow/spring/hivemq/autoconfigure/HiveMQMqttAutoConfiguration.java
+++ hivemq-mqtt-spring-boot-autoconfigure/src/main/java/net/skobow/spring/hivemq/autoconfigure/HiveMQMqttAutoConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class HiveMQMqttAutoconfiguration {
+public class HiveMQMqttAutoConfiguration {
 
     @Bean
     public MqttClient mqttClient() {
